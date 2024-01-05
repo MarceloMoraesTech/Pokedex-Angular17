0fb093a0bdd541def8246141aa31484e76df8ab3
Marcelo Moraes <stereoheart.songs@gmail.com>
2024-01-05 12:11:47 -0300

    Project beginning

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.scss'
+})
+export class PokemonCardComponent {
+}
