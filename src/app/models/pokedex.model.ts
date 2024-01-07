41f3e08a22926c53048fbefd9d41aab99770da95
Marcelo Moraes <stereoheart.songs@gmail.com>
2024-01-07 02:02:11 -0300

    creating components home and not-found

---
+++ src/app/models/pokedex.model.ts
@@ -0,0 +1,21 @@
+﻿export interface Pokemon {
+    abilities: PokemonAbilities[];
+    name: string;
+    id: number;
+    image: string;
+    type: string;
+    weight: number;
+    height: number;
+  }
+  
+  export interface PokemonAbility {
+    name: string;
+    url: string;
+  }
+  
+  export interface PokemonAbilities {
+    ability: PokemonAbility;
+    is_hidden: boolean;
+    slot: number;
+  }
+  
